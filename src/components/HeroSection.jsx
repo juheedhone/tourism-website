652f7148e48e220b445c29c54ca2067f8c4940a9
juheedhone <juheedhone12@gmail.com>
2025-12-18 17:00:31 +0530

    Add HeroSection component with image display

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+      <img className="object-fit w-full" src="./hawa.jpg" alt="hawa">
+     
+      </img>
+    </div>
+  );
+}
+
+export default HeroSection
