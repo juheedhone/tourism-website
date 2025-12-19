60d6ef2ac33b35ab12ee6bb7a59e29b578138775
juheedhone <juheedhone12@gmail.com>
2025-12-19 10:25:32 +0530

    Refactor HeroSection component layout and improve image handling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-
 const HeroSection = () => {
   return (
-    <div>
-      <img className="object-fit w-full" src="./hawa.jpg" alt="hawa">
-     
-      </img>
+    <div className="relative w-full">
+      {/* Background Image */}
+      <img className="w-full  object-cover" src="./hawa.jpg" alt="hawa" />
+
+      {/* Text on top of Image */}
+      <div className="absolute inset-0 flex flex-col items-center justify-top  text-orange-700">
+        <h1 className="text-4xl md:text-6xl font-bold drop-shadow-lg mt-6">
+          Hawa Mahal
+        </h1>
+        <p className="text-lg md:text-2xl mt-3 drop-shadow-lg">
+          Discover the Pink City like never before
+        </p>
+      </div>
     </div>
   );
-}
+};
 
-export default HeroSection
+export default HeroSection;
